56a8209496940ca305f10f1cc9c9d1260d49d5bb
Farzad Abdolhosseini <farzad@fixie.ai>
2024-06-03 10:46:23 -0700

    Audio Encoder to bfloat16 (#4)
    * audio enc to bfloat16
    
    * increase transformers min required version

--- ultravox/training/train.py
+++ ultravox/training/train.py
@@ -174,9 +174,7 @@ def main() -> None:
     logging.info(
         f"Using dtype and device (world_size): {dtype}, {device} ({world_size})"
     )
-    model.to(device)
-    model.language_model.to(dtype)
-    model.multi_modal_projector.to(dtype)
+    model.to(device=device, dtype=dtype)
     # TODO: check if the whole model can now be moved to dtype instead
 
     # Prepare dataset, subsetting if needed
