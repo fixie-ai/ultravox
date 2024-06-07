554a27270ddd8b31c36630a46059979cfd740d71
Farzad Abdolhosseini <farzad@fixie.ai>
2024-06-07 15:44:08 -0700

    cleaning up + adressing comments

--- ultravox/tools/push_to_hub.py
+++ ultravox/tools/push_to_hub.py
@@ -8,20 +8,20 @@ import simple_parsing
 from ultravox.inference import ultravox_infer
 
 
+# This script is used to upload a model to the HuggingFace Hub. It is mainly useful for turning a W&B artifact into a HF model for vLLM use.
+# Ex: python -m ultravox.tools.push_to_hub -m wandb://fixie/ultravox/<model_path> --hf_upload_model fixie-ai/ultravox
 @dataclasses.dataclass
 class UploadToHubArgs:
     # Model ID to use for the model
     model: str = simple_parsing.field(alias="-m")
-    # Audio processor ID to use
-    audio_processor: Optional[str] = None
-    # Tokenizer ID to use
+    # HuggingFace Hub model_id to push to
+    hf_upload_model: str
+    # Tokenizer ID to use: usually the same as the model ID
     tokenizer: Optional[str] = None
-    # Data sets to use for inference
+    # Device to use for the model
     device: Optional[str] = simple_parsing.field(default=None, alias="-D")
     # Data type to use for the model
     data_type: Optional[str] = None
-    # HuggingFace Hub URL to push to
-    hf_upload_url: Optional[str] = None
 
 
 def main(args: UploadToHubArgs):
@@ -29,15 +29,14 @@ def main(args: UploadToHubArgs):
     inference = ultravox_infer.UltravoxInference(
         args.model,
         tokenizer_id=args.tokenizer,
-        audio_processor_id=args.audio_processor,
         device=args.device,
         data_type=args.data_type,
     )
     print("Uploading model to HuggingFace Hub...")
-    inference.model.push_to_hub(args.hf_upload_url)
+    inference.model.push_to_hub(args.hf_upload_model)
     # It's not necessary to upload the tokenizer, but it can be useful for consistency
     print("Uploading tokenizer to HuggingFace Hub...")
-    inference.tokenizer.push_to_hub(args.hf_upload_url)
+    inference.tokenizer.push_to_hub(args.hf_upload_model)
 
 
 if __name__ == "__main__":
