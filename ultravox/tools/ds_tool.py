42251726f2d33a2fe12738f599f25733d3ac3105
juberti <justin@fixie.ai>
2024-06-12 22:03:14 -0700

    Add @file syntax to ds_tool
    Also:
    - makes the task name positional
    - defaults subset to None, as not every default is "default"
    - refactors the map func to be inside the task, making tasks more encapsulated

--- ultravox/tools/ds_tool.py
+++ ultravox/tools/ds_tool.py
@@ -31,6 +31,19 @@ class TtsTask:
         if self.audio_column_name is None:
             self.audio_column_name = f"{self.column_name}_audio"
 
+    def map_split(self, ds_split: datasets.Dataset, num_proc: int) -> datasets.Dataset:
+        def map_sample(sample):
+            text = sample[self.column_name]
+            text = text["text"] if isinstance(text, dict) else text
+            sample[self.audio_column_name] = tts_client.tts(text)
+            return sample
+
+        print(f'TTS mapping "{self.column_name}" to "{self.audio_column_name}"...')
+
+        return ds_split.map(map_sample, num_proc=num_proc).cast_column(
+            self.audio_column_name, datasets.Audio(sampling_rate=self.sample_rate)
+        )
+
 
 @dataclasses.dataclass
 class TextGenerationTask:
@@ -41,16 +54,36 @@ class TextGenerationTask:
     max_tokens: int = 128
     temperature: float = 0
 
+    def __post_init__(self):
+        if self.template.startswith("@"):
+            self.template = open(self.template[1:], "r").read()
+
+    def map_split(self, ds_split: datasets.Dataset, num_proc: int) -> datasets.Dataset:
+        def map_sample(sample):
+            input_text = self.template.format(**sample)
+            response = chat_client.chat.completions.create(
+                model=self.language_model,
+                messages=[{"role": "user", "content": input_text}],
+                max_tokens=self.max_tokens,
+                temperature=self.temperature,
+            )
+            sample[self.new_column_name] = response.choices[0].message.content
+            return sample
+
+        print(
+            f'Text gen for column: "{self.new_column_name}" with template:\n{self.template}'
+        )
+        return ds_split.map(map_sample, num_proc=num_proc)
+
 
 # This script is used to either generate audio samples from text using a TTS model, or to generate text samples using a text generation model.
-# Ex: just ds_tool -t tts -d google/boolq -u fixie-ai/boolq-audio -c question -a audio --token $HF_WRITE_TOKEN
-# Ex: just ds_tool -t textgen -d fixie-ai/boolq-audio -u fixie-ai/boolq-audio -c explanation
-# Ex: just ds_tool -t textgen -d ylacombe/expresso -u fixie-ai/expresso -c continuation \
-#         -T "\"Continue the following sentence in a way that reflects a ‘{style}’ tone in a coherent style:\n{text}\""
+# Ex: just ds_tool tts -d google/boolq -u fixie-ai/boolq-audio -c question -a audio --token $HF_WRITE_TOKEN
+# Ex: just ds_tool textgen -d fixie-ai/boolq-audio -u fixie-ai/boolq-audio -c explanation
+# Ex: just ds_tool textgen -d ylacombe/expresso -u fixie-ai/expresso -c continuation -T @expresso_template.txt
 @dataclasses.dataclass
 class DatasetToolArgs:
     dataset_name: str = simple_parsing.field(alias="-d")
-    dataset_subset: str = simple_parsing.field(default="default", alias="-S")
+    dataset_subset: Optional[str] = simple_parsing.field(default=None, alias="-S")
     dataset_split: Optional[str] = simple_parsing.field(default=None, alias="-s")
 
     num_samples: Optional[int] = simple_parsing.field(default=None, alias="-n")
@@ -100,7 +133,6 @@ def _text_gen_split(
     print(
         f'Text gen for column: "{task.new_column_name}" with template:\n{task.template}'
     )
-    return ds_split.map(_text_gen_sample, num_proc=num_proc)
 
 
 def main(args: DatasetToolArgs):
@@ -109,7 +141,6 @@ def main(args: DatasetToolArgs):
     data_dict: datasets.DatasetDict = datasets.load_dataset(
         ds_name, args.dataset_subset, split=args.dataset_split
     )
-
     if args.dataset_split:
         data_dict = datasets.DatasetDict(**{args.dataset_split: data_dict})
 
@@ -117,16 +148,7 @@ def main(args: DatasetToolArgs):
         print(f'Processing split "{split}"...')
         if args.num_samples:
             ds_split = ds_split.select(range(args.num_samples))
-
-        if isinstance(args.task, TtsTask):
-            data_dict[split] = _tts_split(
-                ds_split, args.task, num_proc=args.num_workers
-            )
-
-        elif isinstance(args.task, TextGenerationTask):
-            data_dict[split] = _text_gen_split(
-                ds_split, args.task, num_proc=args.num_workers
-            )
+        data_dict[split] = args.task.map_split(ds_split, args.num_workers)
 
     token = args.token or os.environ.get("HF_TOKEN")
     hub_args: Dict[str, Any] = {
