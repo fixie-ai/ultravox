e9d4f082c4d3c17dced402200cbc615bbe587348
Farzad Abdolhosseini <farzad@fixie.ai>
2024-06-21 09:20:28 -0700

    fix json_mode in soda creation docs

--- scripts/dataset_creation/soda_audio.sh
+++ scripts/dataset_creation/soda_audio.sh
@@ -7,15 +7,15 @@
 # We want the model to generate the same response whether the input is audio or text
 
 just ds_tool textgen -d allenai/soda --shuffle True -s test -n 1000 -u fixie-ai/soda-audio -c alt_last_turn \
-    -T @ultravox/tools/ds_templates/soda_template.jinja -b https://api.fireworks.ai/inference/v1 \
+    -T @ultravox/tools/ds_templates/soda_template.jinja -j -b https://api.fireworks.ai/inference/v1 \
     -k $FIREWORKS_API_KEY -m accounts/fireworks/models/llama-v3-8b-instruct --token $HF_WRITE_TOKEN
 
 just ds_tool textgen -d allenai/soda --shuffle True -s validation -n 1000 -u fixie-ai/soda-audio -c alt_last_turn \
-    -T @ultravox/tools/ds_templates/soda_template.jinja -b https://api.fireworks.ai/inference/v1 \
+    -T @ultravox/tools/ds_templates/soda_template.jinja -j -b https://api.fireworks.ai/inference/v1 \
     -k $FIREWORKS_API_KEY -m accounts/fireworks/models/llama-v3-8b-instruct --token $HF_WRITE_TOKEN
 
 just ds_tool textgen -d allenai/soda --shuffle True -s train -n 100000 -u fixie-ai/soda-audio -c alt_last_turn \
-    -T @ultravox/tools/ds_templates/soda_template.jinja -b https://api.fireworks.ai/inference/v1 \
+    -T @ultravox/tools/ds_templates/soda_template.jinja -j -b https://api.fireworks.ai/inference/v1 \
     -k $FIREWORKS_API_KEY -m accounts/fireworks/models/llama-v3-8b-instruct --token $HF_WRITE_TOKEN
 
 
