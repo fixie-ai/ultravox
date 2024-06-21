4202b567294f53d2ec42544d9bf0654293f8e464
Farzad Abdolhosseini <farzad@fixie.ai>
2024-06-21 09:00:49 -0700

    BoolQ for Training and Eval (#30)
    * set default to include_context=True
    
    * boolq extended dataset for training
    
    * improved evals + boolq T/F eval + text-only

--- ultravox/evaluation/eval.py
+++ ultravox/evaluation/eval.py
@@ -1,5 +1,6 @@
 from ultravox.evaluation import eval_types
 from ultravox.evaluation import gpt_eval
+from ultravox.evaluation import string_based
 from ultravox.evaluation import wer
 
 
@@ -10,5 +11,7 @@ def evaluate_answer(sample: eval_types.Sample, metric: str) -> eval_types.Result
         return gpt_eval.evaluate_answer_boolq(sample)
     elif metric == "instruct":
         return gpt_eval.evaluate_answer_instruct(sample)
+    elif metric == "exact_match_last_word":
+        return string_based.match_last_word(sample)
     else:
         raise ValueError(f"Unknown metric: {metric}")
