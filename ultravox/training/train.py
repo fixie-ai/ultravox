93fc214aa3b7cfdd932c478d49cb98f5897e0172
Zhongqiang Huang <zhuang@fixie.ai>
2024-08-02 16:39:45 -0700

    Fix require_alt_input bug

--- ultravox/training/train.py
+++ ultravox/training/train.py
@@ -199,7 +199,7 @@ def main() -> None:
                 use_mds=args.mds,
                 mds_batch_size=args.batch_size,
             ),
-            include_alt_fields=model.loss_config.require_alt_input(),
+            include_alt_fields=model.loss_config.require_alt_input,
         )
         val_ds_args = datasets.VoiceDatasetArgs(
             num_prompts=1,
@@ -220,7 +220,7 @@ def main() -> None:
                 processor=processor,
                 num_samples=args.val_num_samples,
                 data_args=val_ds_args_text if k.startswith("text_") else val_ds_args,
-                include_alt_fields=model.loss_config.require_alt_input(),
+                include_alt_fields=model.loss_config.require_alt_input,
             )
             for k in val_sets
         }
@@ -236,7 +236,7 @@ def main() -> None:
     # Set up the data loader
     data_collator = datasets.DataCollatorForSeq2SeqWithAudio(
         tokenizer=text_tokenizer,
-        include_alt_fields=model.loss_config.require_alt_input(),
+        include_alt_fields=model.loss_config.require_alt_input,
     )
 
     logging.info(f"Config Params: {args}")
