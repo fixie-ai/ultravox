5d163f64c7bb28bb6b566f830e9995b8ab101d60
Zhongqiang Huang <zhuang@fixie.ai>
2024-08-08 20:00:56 -0700

    Cleanup code

--- ultravox/inference/base.py
+++ ultravox/inference/base.py
@@ -1,6 +1,6 @@
 import abc
 import dataclasses
-from typing import Generator, Optional, Tuple, Union
+from typing import Generator, Optional, Tuple, Union, List, Dict
 
 from ultravox.data import datasets
 import transformers
@@ -55,3 +55,39 @@ class VoiceInference(abc.ABC):
         output = self.infer(sample, max_tokens, temperature, past_key_values)
         yield InferenceChunk(output.text)
         yield InferenceStats(output.input_tokens, output.output_tokens)
+
+
+class History:
+    def __init__(self, audio_token_replacement: str="<|eot_token|>"):
+        self.audio_token_replacement: str = audio_token_replacement
+        self.audio_placeholder = "<|audio|>"
+        self.messages: List[Dict[str, str]] = []
+        self.key_values: Optional[Union[Tuple, transformers.cache_utils.Cache]] = None
+
+    def add_message(self, message: Dict[str, str], audio_token_len: int):
+        message = message.copy()
+        content = message['content']
+        if audio_token_len > 0:
+            if content.count(self.audio_placeholder) != 1:
+                raise ValueError(f"Expected 1 audio placeholder, found {content.count(self.audio_placeholder)}")
+            message['content'] = content.replace(self.audio_placeholder, self.audio_token_replacement * audio_token_len)
+
+        if self.messages:
+            self.messages.append(message)
+        else:
+            self.messages = [message]
+
+    def update_key_values(self, key_values: Union[Tuple, transformers.cache_utils.Cache]):
+        self.key_values = key_values
+
+    @property
+    def past_messages(self) -> List[Dict[str, str]]:
+        return self.messages
+    
+    @property
+    def past_key_values(self) -> Optional[Union[Tuple, transformers.cache_utils.Cache]]:
+        return self.key_values
+    
+    def reset(self):
+        self.messages = []
+        self.key_values = None
--- ultravox/tools/gradio_demo.py
+++ ultravox/tools/gradio_demo.py
@@ -4,52 +4,30 @@ from typing import Tuple, List, Optional, Union, Dict
 import gradio as gr
 import numpy as np
 import simple_parsing
-import transformers
 
 from ultravox.data import datasets
 from ultravox.inference import ultravox_infer
+from ultravox.inference import base as infer_base
 
+demo_instruction: str="""Enter your prompt here (audio will be inserted at the end or at <|audio|>).
+
+Text mode: Shift+Enter to submit.
+Voice mode: Click the recording button to start, then click again to stop and submit.
+"""
 
 @dataclass
 class DemoConfig:
     model_path: str = "wandb://fixie/ultravox/model-zhuang.2024-07-31-ultravox.blsp-kd-2-tinyllama:v5"
+    # Use <|audio|> to specify where to insert audio, otherwise, audio is inserted at the end in voice mode.
     default_prompt: str = ""
     max_new_tokens: int = 256
     device: str = "mps"
     data_type: str = "float16"
 
-
-class History:
-    def __init__(self, audio_token_replacement: str, audio_placeholder: str = "<|audio|>"):
-        self.past_messages: List[Dict[str, str]] = []
-        self.past_key_values: Optional[Union[Tuple, transformers.cache_utils.Cache]] = None
-        self.audio_token_replacement = audio_token_replacement
-        self.audio_placeholder = audio_placeholder
-
-    def update_messages(self, messages: List[Dict[str, str]], audio_token_len: int):
-        self.past_messages = messages.copy()
-        if audio_token_len > 0:
-            assert self.audio_placeholder in self.past_messages[-1]['content'], "Audio placeholder not found in the last message"
-            self.past_messages[-1]['content'] = self.past_messages[-1]['content'].replace(
-                self.audio_placeholder, self.audio_token_replacement * audio_token_len
-            )
-        else:
-            assert self.audio_placeholder not in self.past_messages[-1]['content'], "Unexpected audio placeholder found"
-
-    def add_assistant_response(self, content: str):
-        self.past_messages.append({'role': "assistant", 'content': content})
-    
-    @property
-    def messages(self):
-        return self.past_messages
-
-
 def main():
-    global args, history, inference
-
-    args = simple_parsing.parse(config_class=DemoConfig)
+    args: DemoConfig = simple_parsing.parse(config_class=DemoConfig)
     inference = ultravox_infer.UltravoxInference(args.model_path, device=args.device, data_type=args.data_type)
-    history = History(audio_token_replacement=inference.tokenizer.bos_token)
+    history = infer_base.History(audio_token_replacement=inference.tokenizer.eos_token)
 
     def add_text(chatbot: gr.Chatbot, text: str) -> gr.Chatbot:
         return chatbot + [(text, None)]
@@ -57,49 +35,67 @@ def main():
     def add_audio(chatbot: gr.Chatbot, audio: str) -> gr.Chatbot:
         return chatbot + [((audio,), None)]
 
-    def disable_interaction():
-        return gr.update(interactive=False), gr.update(interactive=False)
-    
-    def enable_interaction():
-        return gr.update(interactive=True), gr.update(interactive=True)
-
-    def process_turn(chatbot: gr.Chatbot, prompt: str, audio: Optional[Tuple[int, np.ndarray]] = None, num_beams: int = 1, temperature: float = 1.0):
-        updated_prompt = prompt
+    def process_turn(chatbot: gr.Chatbot, prompt: str, audio: Optional[Tuple[int, np.ndarray]] = None, num_beams: int = 1, temperature: float = 0):
+        # We want to keep the prompt (mixed audio/text instruction) as is in voice mode, but set it to "" in anticipation of new prompt in text mode.
+        prompt_to_return = prompt
         if audio:
             if "<|audio|>" not in prompt:
                 prompt += "<|audio|>"
             sample = datasets.VoiceSample.from_prompt_and_file(prompt, audio)
         else:
             sample = datasets.VoiceSample.from_prompt(prompt)
-            updated_prompt = ""
+            prompt_to_return = ""
 
-        sample.add_past_messages(history.messages)
+        if len(sample.messages) != 1:
+            raise ValueError(f"Expected exactly 1 message in sample but got {len(sample.messages)}")
+        user_message = sample.messages[-1]
+        sample.add_past_messages(history.past_messages)
 
         output = inference.infer(
             sample,
             max_new_tokens=args.max_new_tokens,
-            past_key_values=history.past_key_values,
+            past_key_values=history.key_values,
             num_beams=num_beams,
             temperature=temperature
         )
-        history.past_key_values = output.past_key_values
-
-        history.update_messages(sample.messages, output.audio_token_len)
-        history.add_assistant_response(output.text)
+        history.update_key_values(output.past_key_values)
+        # audio_token_len is computed during inference
+        history.add_message(user_message, output.audio_token_len)
+        history.add_message({'role': 'assistant', 'content': output.text}, 0)
 
         chatbot = chatbot + [(None, output.text)]
-        return chatbot, gr.update(value=updated_prompt)
+        return chatbot, gr.update(value=prompt_to_return)
+    
+    def process_text(chatbot, prompt, num_beams, temperature):
+        return process_turn(chatbot, prompt, None, num_beams, temperature)
+
+    def process_audio(chatbot, prompt, audio, num_beams, temperature):
+        return process_turn(chatbot, prompt, audio, num_beams, temperature)
 
     def gradio_reset():
-        global history
-        history = History(audio_token_replacement=inference.tokenizer.bos_token)
+        history.reset()
         return [], "", None
 
     with gr.Blocks() as demo:
-        chatbot = gr.Chatbot(scale=1, height=800)
+        chatbot = gr.Chatbot(scale=10, height=1000)
         
         with gr.Row():
-            with gr.Column(scale=0.1):
+            with gr.Column(scale=1):
+                reset = gr.Button("Reset")
+                audio = gr.Audio(
+                    label="🎤",
+                    sources=["microphone"],
+                    type="filepath",
+                    visible=True,
+                )
+            with gr.Column(scale=8):
+                prompt = gr.Textbox(
+                    show_label=False,
+                    lines=5,
+                    placeholder=demo_instruction,
+                    value=args.default_prompt,
+                    container=True)
+            with gr.Column(scale=1):
                 num_beams = gr.Slider(
                     minimum=1,
                     maximum=10,
@@ -110,54 +106,24 @@ def main():
                 )
                 temperature = gr.Slider(
                     minimum=0,
-                    maximum=2.0,
+                    maximum=5.0,
                     value=0,
                     step=0.1,
                     interactive=True,
                     label="temperature",
                 )
-            with gr.Column(scale=0.8):
-                prompt = gr.Textbox(
-                    show_label=False,
-                    lines=4,
-                    placeholder="Enter text (include <|audio|> to insert audio) and press enter",
-                    value=args.default_prompt,
-                    container=True)
-            with gr.Column(scale=0.1):
-                audio = gr.Audio(
-                    label="🎤",
-                    sources=["microphone"],
-                    type="filepath",
-                    visible=True,
-                )
-                reset = gr.Button("Reset")               
 
-        def process_text(chatbot, prompt, num_beams, temperature):
-            return process_turn(chatbot, prompt, None, num_beams, temperature)
-
-        def process_audio(chatbot, prompt, audio, num_beams, temperature):
-            return process_turn(chatbot, prompt, audio, num_beams, temperature)
-
-        prompt.submit(disable_interaction, [], [prompt, audio], queue=False).then(
-            add_text, [chatbot, prompt], [chatbot], queue=False
+        prompt.submit(add_text, [chatbot, prompt], [chatbot], queue=False
         ).then(
             process_text, [chatbot, prompt, num_beams, temperature], [chatbot, prompt], queue=False
-        ).then(
-            enable_interaction, [], [prompt, audio], queue=False
         )
-
-        audio.stop_recording(disable_interaction, [], [prompt, audio], queue=False).then(
-            add_audio, [chatbot, audio], [chatbot], queue=False
+        audio.stop_recording(add_audio, [chatbot, audio], [chatbot], queue=False
         ).then(
             process_audio, [chatbot, prompt, audio, num_beams, temperature], [chatbot, prompt], queue=False
-        ).then(
-            enable_interaction, [], [prompt, audio], queue=False
         )
-
         reset.click(gradio_reset, [], [chatbot, prompt, audio], queue=False)
 
     demo.launch(share=True)
-
 
 if __name__ == "__main__":
     main()
