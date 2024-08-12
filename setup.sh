863e581abe081babbd9b376e53582f7cfe71811d
Farzad Abdolhosseini <farzad@fixie.ai>
2024-08-12 10:07:36 -0700

    integrate oaievals

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 cd $HOME
 mkdir workspace
 cd workspace
-git clone git@github.com:fixie-ai/ultravox.git -b main
+git clone https://github.com/fixie-ai/ultravox.git -b main
+git clone https://github.com/fixie-ai/evals.git -b main
 cd ultravox
 mkdir -p ~/.local/bin
 curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | bash -s -- --to ~/.local/bin
 just install
+poetry run pip install -e ../evals
 bash ./scripts/vscode_tunnel.sh
