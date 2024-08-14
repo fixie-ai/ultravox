71bf27e94f83a7d0129b19cf6864f6579c9e195b
Patrick Li <patrick8289@gmail.com>
2024-08-14 14:35:12 -0700

    Add some print statements

--- ultravox/tools/infer_tool.py
+++ ultravox/tools/infer_tool.py
@@ -196,6 +196,7 @@ def dataset_infer(inference: base.VoiceInference, args: InferArgs):
     if args.batch_size:
         start_time = time.time()
         print("batch size", args.batch_size)
+
         samples = []
         for i, sample in enumerate(datasets.Range(ds, args.num_samples)):
             samples.append(sample)
