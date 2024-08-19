d6845df6eac7258124febf9a497afb10c4015f9d
juberti <justin@fixie.ai>
2024-08-19 16:23:58 -0700

    sr

--- ultravox/inference/utils.py
+++ ultravox/inference/utils.py
@@ -5,9 +5,7 @@ def default_device():
     return (
         "cuda"
         if torch.cuda.is_available()
-        else "mps"
-        if torch.backends.mps.is_available()
-        else "cpu"
+        else "mps" if torch.backends.mps.is_available() else "cpu"
     )
 
 
@@ -19,7 +17,5 @@ def get_dtype(data_type: str):
     return (
         torch.bfloat16
         if data_type == "bfloat16"
-        else torch.float16
-        if data_type == "float16"
-        else torch.float32
+        else torch.float16 if data_type == "float16" else torch.float32
     )
